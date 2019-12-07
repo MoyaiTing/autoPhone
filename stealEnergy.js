60d999624c4523f94bfb6cd444b0ccb38fd5ea4f
p_moshiyuan <1085538602@qq.com>
2019-12-07 21:51:07 +0800

    大版本更新，少了很多功能，改为坐标收集能量

--- stealEnergy.js
+++ stealEnergy.js
@@ -5,43 +5,90 @@ const DeviceMoy = require("./comm/utils/DeviceMoy.js");
 
 auto();
 setScreenMetrics(1080, 1920);
-let shoujishijian = rawInput("请输入收集时间", "07:00") || "07:02";
-let options = ["稳定版", "激进版"];
-let type = dialogs.select("请选择一个选项", options);
-if (type < 0) {
-    type = '0';
+
+let getMySumEnergy=0;//获取自己总能量
+let getOrderSumEnergy=0;//获取他人总能量
+const isTest=1;
+log('执行时间：'+Util.getCurDate());
+try {
+    initAuto();
 }
-log("执行时间：" + shoujishijian);
-log("执行版本：" + options[type]);
-let index = 0;
-let xunhuanshijian;
-let lastXunhuanshijian = Util.getCurDate('hh:mm');
-while (1 == 1) {
-    index += 1;
-    xunhuanshijian = Util.getCurDate('hh:mm');
-    if (lastXunhuanshijian != xunhuanshijian) {
-        //获取时间差
-        let betweenTime = parseInt(xunhuanshijian.split(':')[1]) - parseInt(lastXunhuanshijian.split(':')[1]);
-        //如果时间差超过了1分钟，则唤醒屏幕，重置一下sleep
-        if (betweenTime > 1||(betweenTime<0&&betweenTime>-59)) {
-            log("\n当前循环次数：" + index + "\n上次循环时间：" + lastXunhuanshijian+ "\n当前时间：" + xunhuanshijian);
-            DeviceMoy.wakeUpLittleTime(30 * 1000);
-        }
-    }
-    lastXunhuanshijian = xunhuanshijian;
-    if (xunhuanshijian.split(':')[0] == shoujishijian.split(':')[0] && xunhuanshijian.split(':')[1] >= shoujishijian.split(':')[1]) {
-        log("开始收集能量");
-        DeviceMoy.jiesuo();
-        let getEnergy = shoujinengliang(type);
-        //收集能量为0，再收集一次，防止弹窗
-        if (!getEnergy || getEnergy == "0") {
-            shoujinengliang(type);
-        }
-        DeviceMoy.suoping();
-        break;
-    }
-    sleep(200);
+catch(err) {
+    log(err);
+    log("执行报错，再来一次");
+    back();
+    sleep(300);
+    back();
+    sleep(300);
+    back();
+    sleep(300);
+    back();
+    sleep(300);
+    initAuto();
 }
+
+
+function initAuto(){
+    if(isTest){
+        likeZhixing('1');
+        return;
+    } 
+    let options = ["稳定版", "激进版"];
+    let type = dialogs.select("请选择一个选项", options);
+    if (type < 0) {
+        type = '0';
+    }
+    let options1 = ["立刻执行", "定时执行"];
+    let type1 = dialogs.select("请选择一个选项", options1);
+    if (type1 < 0) {
+        type1 = '0';
+    }
+    if(type1 == '0'){
+        likeZhixing(type);
+    }else{
+        dingshiZhixing();
+    }
+    log("执行版本：" + options[type]);
+}
+
+function dingshiZhixing(){
+    let shoujishijian = rawInput("请输入收集时间", "07:00") || "07:02";
+    log("执行时间：" + shoujishijian);
+    let index = 0;
+    let xunhuanshijian;
+    let lastXunhuanshijian = Util.getCurDate('hh:mm');
+    while (1 == 1) {
+        index += 1;
+        xunhuanshijian = Util.getCurDate('hh:mm');
+        if (lastXunhuanshijian != xunhuanshijian) {
+            //获取时间差
+            let betweenTime = parseInt(xunhuanshijian.split(':')[1]) - parseInt(lastXunhuanshijian.split(':')[1]);
+            //如果时间差超过了1分钟，则唤醒屏幕，重置一下sleep
+            if (betweenTime > 1||(betweenTime<0&&betweenTime>-59)) {
+                log("\n当前循环次数：" + index + "\n上次循环时间：" + lastXunhuanshijian+ "\n当前时间：" + xunhuanshijian);
+                DeviceMoy.wakeUpLittleTime(30 * 1000);
+            }
+        }
+        lastXunhuanshijian = xunhuanshijian;
+        if (xunhuanshijian.split(':')[0] == shoujishijian.split(':')[0] && xunhuanshijian.split(':')[1] >= shoujishijian.split(':')[1]) {
+            log("开始收集能量");
+            DeviceMoy.jiesuo();
+            likeZhixing(type);
+            DeviceMoy.suoping();
+            break;
+        }
+        sleep(200);
+    }
+}
+
+function likeZhixing(type){
+    let getEnergy = shoujinengliang(type);
+    //收集能量为0，再收集一次，防止弹窗
+    if (getEnergy===0) {
+        shoujinengliang(type);
+    }
+}
+
 
 function shoujinengliang(type) {
     let staStamp = (new Date()).getTime();
@@ -54,84 +101,69 @@ function shoujinengliang(type) {
     let mayi = text("蚂蚁森林").findOne(2000);
     if (mayi) {
         mayi.parent().parent().click();
+    }else{
+        back();
+        return 0;
     };
     sleep(8000);
-    log('记录当前能量');
-    sleep(1000);
-    let staEnergy = (descEndsWith('g').boundsInside(750, 245, 950, 350).findOne(8000) || {}).desc() || "0";
-    log("当前能量为"+staEnergy);
-    sleep(1000);
+
     log('收集自己的能量');
-    let energyList = descContains('收集能量').find() || [];
-    energyList.forEach(res => {
-        click(res.bounds().centerX(), res.bounds().centerY());
-        sleep(500);
-    })
-    let staEnergy1 = (descEndsWith('g').boundsInside(750, 245, 950, 350).findOne(8000) || {}).desc() || "0";
-    log("收集到自己的能量为"+(parseInt(staEnergy1)-parseInt(staEnergy))+"g");
+    getMySumEnergy=clickEnergyBall();
+    log("收集到自己的能量为"+getMySumEnergy+"g");
     sleep(2000);
     log('收集别人的能量');
     //稳定版
     if (type == 0) {
-        let otherEnergyList = descEndsWith('kg').className('android.view.View').clickable(true).find() || [];
+        let otherEnergyList = textEndsWith('kg').className('android.view.View').clickable(true).find() || [];
         otherEnergyList.forEach(res1 => {
             log("当前被偷取人：" + getEnergyPeopleName(res1,type));
             res1.click();
             //点击能量球
-            clickEnergyBall();
+            getOrderSumEnergy=clickEnergyBall();
             sleep(500);
             back();
             sleep(1000);
         })
         //激进版
     } else {
-        let look = desc("查看更多好友").findOne(1500);
+        let look = text("查看更多好友").findOne(1500);
         if (!look) {
             back();
-            look = desc("查看更多好友").findOne(1500) || {};
+            look = text("查看更多好友").findOne(1500) || {};
         }
         look.click();
         sleep(5000);
-        let otherEnergyList = descEndsWith('g').className('android.view.View').clickable(true).find() || [];
-        let length = otherEnergyList.length;
-        let otherEnergyList1;
-        let otherEnergy;
-        let staYY;
-        if (length > 0) {
-            staYY = otherEnergyList[0].bounds().centerY();
-        }
+        let length = 76;
         for (let i = 0; i < length; i++) {
-            otherEnergyList1 = descEndsWith('kg').className('android.view.View').clickable(true).find() || [];
-            otherEnergy = otherEnergyList1[i] || {};
-            log("当前被偷取人：" + getEnergyPeopleName(otherEnergy,type));
-            let bounds = otherEnergy.bounds() || {};
-            length = otherEnergyList1.length;
-            let XX = bounds.centerX();
-            let YY = bounds.centerY();
+            let XX=140;
+            let YY=680;
+            let chazhi = 193;
             click(XX, YY);
+            sleep(500);
+            //再滚动一下，防止刚好自己的时候没有跳转
+            swipe(140, 540 + chazhi, 140, 540, 400);
+            click(XX, YY);
+
             //点击能量球
-            clickEnergyBall();
+            getOrderSumEnergy+=clickEnergyBall();
             sleep(200);
             back();
             sleep(300);
-            let chazhi = YY - staYY;
-            swipe(540, 181 + 540 + chazhi, 540, 540, 400);
+            swipe(140, 540 + chazhi, 140, 540, 400);
             sleep(300);
         }
         back();
         sleep(1000);
     }
+    log("偷取别人能量："+getOrderSumEnergy+"g");
     sleep(1000);
     log('记录最后能量');
     let endStamp = (new Date()).getTime();
     let costStamp = staStamp - endStamp;
     let costTime = parseInt(costStamp / 1000 / 60) + "分" + (parseInt(costStamp / 1000) % 60) + "秒";
-    let endEnergy = (descEndsWith('g').boundsInside(750, 245, 950, 350).findOne(3000) || {}).desc() || "0";
-    let getEnergy = (parseInt(staEnergy) - parseInt(endEnergy));
-    log("偷取别人能量："+(parseInt(staEnergy1) - parseInt(endEnergy))+"g");
     Tools.ServerMessage.sendMessage(
         "偷能量",
-        "开始能量：" + staEnergy + "*****结束能量：" + endEnergy + "*****获得能量：" + getEnergy + "g*****耗费时长：" + costTime
+        "一共收取自己能量：" + getMySumEnergy + "*****他人能量：" + getOrderSumEnergy + "*****获得总能量：" + (getMySumEnergy+getOrderSumEnergy) + "g*****耗费时长：" + costTime
     );
     log("退出支付宝");
     back();
@@ -142,35 +174,22 @@ function shoujinengliang(type) {
     sleep(300);
     back();
     sleep(300);
-    return getEnergy;
+    return (getMySumEnergy+getOrderSumEnergy);
 };
 
 //点击能量球
 function clickEnergyBall(){
     sleep(3000);
-    let energyStart=0;
-    try {
-        energyStart=descContains('你收取TA').findOne(1000).parent().findOne(descContains('g')).desc();
-    }
-    catch(err) {
-        log('没有找到偷取能量');
-    }
-    let _energyList = descContains('收集能量').find() || [];
-    let energySum=0;
+    let _energyList = textContains('收集能量').find() || [];
     sleep(200);
+    let getEnergy=0;
     _energyList.forEach(res => {
-        energySum+=parseInt(res.desc());
+        getEnergy+=parseInt(res.text());
         click(res.bounds().centerX(), res.bounds().centerY());
         sleep(500);
     });
-    let energyEnd=0;
-    try {
-        energyEnd=descContains('你收取TA').findOne(1000).parent().findOne(descContains('g')).desc();
-    }
-    catch(err) {
-        log('没有找到偷取能量');
-    }
-    log("可偷能量："+energySum+"g，一共偷的能量："+(parseInt(energyEnd)-parseInt(energyStart))+'g');
+    log("一共获取能量："+getEnergy+'g');
+    return getEnergy;
 }
 
 //获取被偷取人的姓名
@@ -179,10 +198,10 @@ function getEnergyPeopleName(res,type){
     try {
         if(type==0){
             //前三个和后面的位置不一样
-            energyPeopleName=(res.parent().child(1).child(0)||res.parent().child(2).child(0)).desc();
+            energyPeopleName=(res.parent().child(1).child(0)||res.parent().child(2).child(0)).text();
         }else{
             //前三个和后面的位置不一样
-            energyPeopleName=res.parent().child(1).desc()||res.parent().child(2).desc();
+            energyPeopleName=res.parent().child(1).text()||res.parent().child(2).text();
         }
     }
     catch(err) {
